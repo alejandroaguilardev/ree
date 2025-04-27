c716b472e832a6b8d485380b64544a8ce1142c6e
Alejandro <alexaguilar281@gmail.com>
2025-04-27 15:43:56 +0200

    feat: :white_check_mark: balance resolver

---
+++ apps/server/test/balance/balance.resolver.spec.ts
@@ -0,0 +1,48 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { BalanceResolver } from '../../src/balance/balance.resolver';
+import { GetBalanceDto } from '../../src/balance/infraestructura/dto/get-balance.dto';
+import { BalanceService } from '../../src/balance/services/balance.service';
+import { ExternalBalanceMother } from './domain/external-balance.mother';
+
+describe('BalanceResolver', () => {
+    let resolver: BalanceResolver;
+    let balanceService: BalanceService;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                BalanceResolver,
+                {
+                    provide: BalanceService,
+                    useValue: {
+                        findByDateRange: jest.fn(),
+                    },
+                },
+            ],
+        }).compile();
+
+        resolver = module.get<BalanceResolver>(BalanceResolver);
+        balanceService = module.get<BalanceService>(BalanceService);
+    });
+
+
+    describe('getBalanceByDateRange', () => {
+        it('should return balance data by date range', async () => {
+            const balanceData = {
+                date: new Date('2025-04-25'),
+            };
+
+            const { startDate, endDate } = ExternalBalanceMother.generateQueryString();
+            const balanceDto: GetBalanceDto = {
+                startDate: new Date(startDate),
+                endDate: new Date(endDate),
+            };
+
+            jest.spyOn(balanceService, 'findByDateRange').mockResolvedValue(balanceData);
+
+            const result = await resolver.getBalanceByDateRange(balanceDto);
+            expect(result).toEqual(balanceData);
+            expect(balanceService.findByDateRange).toHaveBeenCalledWith(balanceDto.startDate, balanceDto.endDate);
+        });
+    });
+});
